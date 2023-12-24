b48202190c75958da361584a0071f6dfea2fc0c7
theodenegre <108814494+theodenegre@users.noreply.github.com>
2023-12-24 10:37:29 +0100

    Semms to work

---
+++ UI.py
@@ -0,0 +1,84 @@
+from tkinter import *
+from tkinter import ttk
+from game import My2048
+
+root = Tk()
+root.title("2048")
+WIDTH = 780
+HEIGHT = 780
+root.geometry(f"{WIDTH}x{HEIGHT}")
+root.resizable(False, False)
+root.config(bg="#faf8ef")
+
+colors = {
+    0   : "#cdc1b4",
+    2   : "#eee4da",
+    4   : "#ede0c8",
+    8   : "#f2b179",
+    16  : "#f59563",
+    32  : "#f67c5f",
+    64  : "#f65e3b",
+    128 : "#edcf72",
+    256 : "#edcc61",
+    512 : "#edc850",
+    1024: "#edc53f",
+    2048: "#edc22e",
+    4096: "#3c3a32"
+}
+
+game = My2048(size=4)
+game.start()
+
+# make the grid of the game
+for i in range(game.size):
+    for j in range(game.size):
+        padding = 5
+        frame = Frame(root,
+                      width=(WIDTH - (2 * game.size * padding)) / game.size,
+                      height=(HEIGHT - (2 * game.size * padding)) / game.size,
+                      bg=colors[game.board[i][j]])
+        frame.grid(row=i, column=j, padx=5, pady=5)
+        label = Label(frame, text=game.board[i][j],
+                      font=("Arial", 40, "bold"),
+                      bg=colors[game.board[i][j]], fg="#776e65")
+        label.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+# Create a StringVar to store the pressed key
+pressed_key = StringVar()
+
+
+# Function to handle key press events
+def on_key_press(event):
+    pressed_key.set(event.keysym)
+
+root.bind("<Key>", on_key_press)
+
+while True:
+    root.update()
+    if game.is_over():
+        break
+    if game.is_win():
+        break
+
+    # Wait for a key to be pressed
+    root.wait_variable(pressed_key)
+    game.move(pressed_key.get())
+    # Clear the pressed key for the next iteration
+    pressed_key.set("")
+
+    for i in range(game.size):
+        for j in range(game.size):
+            padding = 5
+            frame = Frame(root,
+                          width=(WIDTH - (
+                                      2 * game.size * padding)) / game.size,
+                          height=(HEIGHT - (
+                                      2 * game.size * padding)) / game.size,
+                          bg=colors[game.board[i][j]])
+            frame.grid(row=i, column=j, padx=5, pady=5)
+            label = Label(root, text=game.board[i][j],
+                          font=("Arial", 40, "bold"),
+                          bg=colors[game.board[i][j]], fg="#776e65")
+            label.grid(row=i, column=j, padx=5, pady=5)
+
+root.mainloop()
---
+++ game.py
@@ -0,0 +1,135 @@
+from copy import deepcopy
+from math import ceil
+from random import randint
+
+
+def create_mat(size):
+    return [[0 for _ in range(size)] for _ in range(size)]
+
+
+class My2048:
+    def __init__(self, size):
+        self.size = size
+        self.board = create_mat(self.size)
+        self.last_matrice = deepcopy(self.board)
+        self.start()
+        self.won = False
+        self.score = 0
+
+    def __str__(self):
+        res = ""
+        for y in range(self.size):
+            for x in range(self.size):
+                res += str(self.get_elem(x, y)) + " "
+            res += "\n"
+        return res
+
+    def change_elem(self, x, y, val):
+        self.board[y][x] = val
+
+    def get_elem(self, x, y):
+        return self.board[y][x]
+
+    def spawn_number(self):
+        x, y = randint(0, self.size - 1), randint(0, self.size - 1)
+        while self.get_elem(x, y) != 0:
+            x, y = randint(0, self.size - 1), randint(0, self.size - 1)
+        self.change_elem(x, y, 2)
+
+    def start(self):
+        for _ in range(ceil(self.size ** 0.5)):
+            self.spawn_number()
+
+    def move(self, d):
+        d = d.lower()
+        if d == "up":
+            self.moveUp()
+        elif d == "down":
+            self.moveDown()
+        elif d == "left":
+            self.moveLeft()
+        elif d == "right":
+            self.moveRight()
+        else:
+            raise ValueError("d must be in {Up, Down, Left, Right}")
+
+    def moveUp(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] != 0:
+                    k = i - 1
+                    while k >= 0 and self.board[k][j] == 0:
+                        k -= 1
+                    if k >= 0 and self.board[k][j] == self.board[i][j]:
+                        self.board[k][j] *= 2
+                        self.board[i][j] = 0
+                        self.score += self.board[k][j]
+                    elif k + 1 != i:
+                        self.board[k + 1][j] = self.board[i][j]
+                        self.board[i][j] = 0
+        print(self.board)
+        print(self.last_matrice)
+        if self.last_matrice != self.board:
+            self.spawn_number()
+            self.last_matrice = deepcopy(self.board)
+
+    def transpose(self):
+        for i in range(self.size):
+            for j in range(i, self.size):
+                self.board[i][j], self.board[j][i] = self.board[j][i], \
+                    self.board[i][j]
+
+    def reverseLR(self):
+        for ligne in self.board:
+            ligne.reverse()
+
+    def reverseUD(self):
+        self.transpose()
+        self.reverseLR()
+        self.transpose()
+
+    def moveDown(self):
+        self.reverseUD()
+        self.moveUp()
+        self.reverseUD()
+
+    def moveLeft(self):
+        self.transpose()
+        self.moveUp()
+        self.transpose()
+
+    def moveRight(self):
+        self.reverseLR()
+        self.moveLeft()
+        self.reverseLR()
+
+    def is_over(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] == 0 or \
+                        (i > 0 and self.board[i][j] == self.board[i - 1][j]) or \
+                        (j > 0 and self.board[i][j] == self.board[i][j - 1]):
+                    return False
+        return True
+
+    def is_win(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] == 2048:
+                    return True
+        return False
+
+if __name__ == "__main__":
+    game = My2048(4)
+    game.board = \
+        [[2, 2, 0, 0],
+         [4, 0, 0, 0],
+         [0, 0, 0, 0],
+         [2, 0, 2, 0]]
+    print(game)
+
+    for move in ["Down", "Left", "Up", "Right"]:
+        game.move(move)
+        print(game)
+        print("Score:", game.score)
+        print("")
